e402e0a7881876c970da6a9b1fe071ed067aea1c
Ishan Zuaim <ishanzuaim@gmail.com>
2024-02-02 18:43:30 +1030

    New config based on lazyvim.

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,15 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "ggandor/leap.nvim",
+    enabled = false,
+  },
+  {
+    "ggandor/flit.nvim",
+    enabled = false,
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+}
